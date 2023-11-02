a5304d3934a33915a7b3866bc7e875f262e5ef0d
Alfredo Lozano <alfredo.realizador@gmail.com>
2023-11-02 21:00:18 +0100

    Create some test

--- src/assets/hooks/phoneHook.test.tsx
+++
@@ -1,8 +0,0 @@
-// import { screen, render, act } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-// import { userEvent } from '@testing-library/user-event';
-// import { useTasks } from './use.tasks';
-// import { renderHook, act } from '@testing-library/react-hooks';
-// import { usePhone } from './usePhone';
-
-// describe('Given a usePhone hook'());
